84d8575839901c3f81f9cab41d9b3cc0e651e960
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-04-10 18:55:44 +0300

    added firebase config

---
+++ src/utils/firebase.ts
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { getApps, initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { GoogleAuthProvider } from 'firebase/auth/cordova'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: 'chatty-150ea.firebaseapp.com',
+  projectId: 'chatty-150ea',
+  storageBucket: 'chatty-150ea.appspot.com',
+  messagingSenderId: '570388071248',
+  appId: '1:570388071248:web:e9efbe3a5909900cd0a255',
+  measurementId: 'G-456RYBQW2R',
+}
+
+// Initialize Firebase
+const app =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
+
+const db = getFirestore(app)
+const auth = getAuth(app)
+const storage = getStorage(app)
+const provider = new GoogleAuthProvider()
+
+export { auth, db, provider, storage }
